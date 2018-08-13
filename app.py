6a162e25a87edaf260f2aa4fc66d9666b10360b0
Unicorn Rentals <elasticvolcano@gmail.com>
2018-08-13 02:34:26 +0000

    DO YOU KNOW HOW TO TURN OFF CAPSLOCK?

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = 'ab4c58142c2cccc1dfa1ee3384c6c44b'
-__version__ = '2c1d8c33023041d01b6e1eec6a7e229b'
+__vcs_id__ = 'd969d147dd1cbdf36d1e31e77c1af5d8'
+__version__ = '3de61163a147a5898537d544872fcdee'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
